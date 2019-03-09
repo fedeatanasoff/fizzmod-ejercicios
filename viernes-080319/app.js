5f499be2fd9ae01a4f4dbc01efb027e496e835ff
fedeatanasoff <fede.atanasoff@gmail.com>
2019-03-08 21:54:53 -0300

    terminando sockets

--- viernes-080319/app.js
+++ viernes-080319/app.js
@@ -25,7 +25,7 @@ let nuevo = fs.createWriteStream(__dirname+"output.txt")
 // * cuando nos llega un chunk a traves del stream en forma de buffer
 // * todos los streams del tipo readable tienen el evento 'data'
 archivo.on('data', chunk => {
-    console.log('nuevo chunk: ', chunk );
+    // console.log('nuevo chunk: ', chunk );
     /*
     * todos los streams del tipo writable implementan el metodo
     * write para escribir por stream
@@ -34,12 +34,12 @@ archivo.on('data', chunk => {
 })
 
 // * https://medium.freecodecamp.org/node-js-streams-everything-you-need-to-know-c9141306be93
-
+// * https://elabismodenull.wordpress.com/2017/03/28/el-manejo-de-streams-en-nodejs/
 /*
  * TCP/IP
  * HTTP 
  *  
- * Request (cliente)
+ * Request (cliente) -------------------------------
  * METODO - URL - VERSION
  * Headers
  * body
@@ -47,5 +47,56 @@ archivo.on('data', chunk => {
  * GET /index.html http/1.1
  * host : www.google.com
  * Content-type : text/plain
+ * 
+ * Respuesta (servidor) -----------------------------
+ * VERSION CODIGO MENSAJE
+ * headers
+ * body
+ * http:/1,1 200 ok
+ * content-type: text/plain
+ * content-lenght: 124
+ * Access-content-allow-origin: *
+ * Access-content-allow-origin: google.com
+ * Access-content-allow-origin: localhost
+ * 
+ * CORS: cross origin resource sharing es una politica de
+ * seguridad para compartir recursos a traves de dominios
+ * 
  */
 
+ // * PIPES - inicias un streaming desde un recurso y la redirigis a su output
+//  leo - redirijo - output
+// archivo.pipe(nuevo)
+
+
+// * modulo NET: sirve para hacer servidores de conexion TCP *  
+const net = require('net')
+// * en un servidor de tcp recibimos una instancia de net.socket
+// * el cual implementa la interfaz de stream duplex
+// * osea que podemos consumirlo y escribirlo
+
+let mensajes = []
+
+const servidor = net.createServer( socket => {
+    
+    mensajes.push(socket)
+    /*
+     * en un servidor TCP o HTTP siempre hay que cerrar la respuesta 
+     * del cliente, de lo contrario el mismo queda en Timeout 
+     */
+     socket.write('Estableciendo conexion \n\r')
+     socket.write('Bienvenido \n\r')
+    socket.on('data', chunk => {
+        console.log('consola servidor: ', chunk.toString())
+        socket.write('conexion telnet: \n\r', chunk)
+
+        for(let index = 0; index < mensajes.length; index ++){
+            const s = index[index]
+            s.write(chunk)
+        }
+    })
+
+    //  socket.end('cerrando conexion')
+
+    
+}).listen(8000, ()=> console.log('iniciando servidor en el puerto 8000'))
